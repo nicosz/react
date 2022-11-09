a505e40ba8d469bd356edfd263b3e5a4cdec63e3
nicolas sztajnchamer <nicosteinjamer@gmail.com>
2022-11-09 20:55:43 -0300

    reaguste de link

--- src/components/ItemList/Item.jsx
+++ src/components/ItemList/Item.jsx
@@ -3,17 +3,17 @@ import Mybutton from "../mybutton/Mybutton";
 import { Link } from "react-router-dom";
 const Item = ({ producto }) => {
   return (
-    <Link to={`/producto/${producto.id}`}>
-      <div className="card">
+    <div className="card">
         <img className="image" src={producto.image} alt={producto.title} />
-        <div>
-          <span>{producto.title}  </span>
-          <p> USD {producto.price}</p>
-          <p>{producto.category}</p>
-          <Mybutton stock={producto.stock} title="Agregar al Carrito" color="green" />
-        </div>
+      <div>
+      <Link to={`/producto/${producto.id}`}>
+        <span>{producto.title}  </span>
+        <p> USD {producto.price}</p>
+      </Link>
+        <p>{producto.category}</p>
+        <Mybutton stock={producto.stock} title="Agregar al Carrito" color="green" />
       </div>
-    </Link>
+    </div>
   )
 }
 
